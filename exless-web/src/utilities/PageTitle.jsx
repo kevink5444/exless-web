560171b317702372809a075900d9b5fc3da5ef31
kevink5444 <kevinkurniawan901@gmail.com>
2023-07-23 10:35:22 +0700

    test

--- exless-web/src/utilities/PageTitle.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function PageTitle(props) {
-  return (
-    <h3 className='p-5 text-3xl bg-white font-bold pl-4 pb-4'>{props.title}</h3>
-  )
-}
-
-export default PageTitle
